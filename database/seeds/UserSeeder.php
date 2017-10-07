3ab59939dc52ea20adaced5129df2de40a7115ce
Wim Verpoorten <wim.verpoorten@hotmail.com>
2017-10-07 13:33:21 +0200

    UserSeeder added & tested + added to DatabaseSeeder.

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
 
     public function run()
     {
-        $this->call(UserSeeder::class);
+        factory(App\User::class, 10)->create();
     }
 }
