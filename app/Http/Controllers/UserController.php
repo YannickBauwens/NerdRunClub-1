20e9c758bf2ed68a6f4e8f2826dedc8a38a74e49
Lisa9W <lisa@m-release.com>
2017-10-07 11:17:59 +0200

    Able to get access_token

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,24 +15,27 @@ class UserController extends Controller
 
     public function token_exchange()
     {
+        // token uit url halen
         $token = request()->code;
-
-        //https://www.strava.com/oauth/{token}
+        // Guzzle client opzetten = naar wie ga je requests sturen
         $client = new Client([
             // Base URI is used with relative requests
-            'base_uri' => 'https://www.strava.com',
+            'base_uri' => 'https://www.strava.com/oauth/',
             // You can set any number of default request options.
             'timeout'  => 2.0,
         ]);
 
-        $r = $client->request('POST', 'https://www.strava.com/token', [
+        // Http request uitvoeren, we geven client_id, client_secret en de verkregen code mee als parameter
+        $r = $client->request('POST', 'token', [
             'form_params' => [
                 'client_id' => env('STRAVA_ID'),
                 'client_secret' => env('STRAVA_SECRET'),
                 'code' => $token
             ]
         ]);
-
-        dd($r);
+        $result = json_decode ($r->getBody()->getContents());
+        $access_token = $result->{'access_token'};
+        $username = $result->{'athlete'}->{'username'};
+        dd($username);
     }
 }
