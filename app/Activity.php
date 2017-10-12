e6539f8dc1232d6537e675c806b731f440c642be
Wim Verpoorten <wim.verpoorten@hotmail.com>
2017-10-12 20:55:21 +0200

    Activity factory added & tested

--- app/Activity.php
+++ app/Activity.php
@@ -2,9 +2,28 @@
 
 namespace App;
 
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model;
 
 class Activity extends Model
 {
-    //
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'strava_activity_id', 'athlete_id', 'distance', 'start_date',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+
+    ];
 }
