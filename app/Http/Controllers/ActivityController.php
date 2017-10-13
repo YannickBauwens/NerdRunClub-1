a862aa04288f9117e38248b90431704d44bf403b
Wim Verpoorten <wim.verpoorten@hotmail.com>
2017-10-13 16:29:55 +0200

    Activities from API OK (raw data only)

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Activity;
+use App\Strava;
 use Illuminate\Http\Request;
 
 class ActivityController extends Controller
@@ -10,6 +11,18 @@ class ActivityController extends Controller
     public function index()
     {
         $user = auth()->user();
+        $token = $user->token;
+
+        $strava = new Strava();
+        $a = $strava->client->request('GET', '/api/v3/athlete/activities', [
+            'headers' => [
+                'Authorization' => 'Bearer '.$token
+            ]
+        ]);
+        $result = json_decode($a->getBody()->getContents());
+        dd ($result);
+
+
         $activities = Activity::all()->where('strava_id', $user->strava_id);
         return view('activities', ['strava_id' => $user->strava_id, 'firstname' => $user->firstname, 'activities' =>
             $activities]);
