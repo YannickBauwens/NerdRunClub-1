407b614048a474740c64dedfb1a3e1f3ae2a7c5a
Wim Verpoorten <wim.verpoorten@hotmail.com>
2017-10-13 18:20:28 +0200

    deel2 vorige commit

--- resources/views/activities.blade.php
+++ resources/views/activities.blade.php
@@ -3,5 +3,6 @@
 <p>Hi {{ $firstname }}, who has id {{ $strava_id }}, these are your activities!</p>
 
 @foreach ($activities as $activity)
-    <p>This is activity {{ $activity->id }}</p>
+    <p>This is activity {{ $activity->id }}, in which you ran {{ $activity->distance }} meters on {{
+    $activity->start_date }}.</p>
 @endforeach
